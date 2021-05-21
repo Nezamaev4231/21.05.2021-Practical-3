b07a1ce513bbbdf8f6ea913ed05d6322face6fd3
unknown <you@example.c>
2021-05-21 11:48:52 +0300

    21.05 пр3

---
+++ 17.05.2021 ПР3/17.05.2021 ПР3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._05._2021_ПР3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //9.2
+
+            Console.WriteLine("Введите x");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите k");
+            double k = Convert.ToInt32(Console.ReadLine());
+
+            double y = 0;
+            if (k < x)
+            {
+               y= k * x;
+            }
+            else
+            {
+                y = k + x;
+            }
+            Console.WriteLine("y="+y);
+            Console.ReadKey();
+           
+
+        }
+    }
+}
---
+++ 17.05.2021 ПР3/9.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //9.1 пр3
+       
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            while(n!= 0)
+
+            {
+                int a = n % 10;
+                if (a == 3)
+                {
+                    k++;
+                }
+                n = n / 10;
+            }
+            if (k == 0)
+            {
+                Console.WriteLine("нет");
+            }
+            else
+            {
+                Console.WriteLine("да");
+            }
+            Console.ReadKey();
+
+
+           
+        }
+    }
+}
